397b84b9157bcc5ee44a53cfecf2da98a8cdb948
Ayub <ayyub.amini@gmail.com>
2021-06-24 23:31:49 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BinaryGapSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a positive integer: ");
+
+            var input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The result is: {0}", GetBinaryGap(Convert.ToString(input, 2).ToCharArray()));
+        }
+
+        private static int GetBinaryGap(char[] binaryString)
+        {
+            var checkSum = 0;
+            var zeroCount = 0;
+
+            for (var i = 0; i < binaryString.Length; i++)
+            {
+                if (binaryString[i] == '1')
+                {
+                    checkSum++;
+                }
+            }
+
+            if (checkSum == 0 || checkSum == 1)
+            {
+                return 0;
+            }
+
+            return binaryString.Length;
+        }
+    }
+}
